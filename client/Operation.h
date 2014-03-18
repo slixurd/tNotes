d00b945d6de20e03a7b87581b7d900d90efbeae8
lpp <522623259@qq.com>
2014-03-18 16:26:28 +0800

    修改Operation.h

--- client/Operation.h
+++ client/Operation.h
@@ -10,13 +10,15 @@
 #ifndef OPERATION_H
 #define OPERATION_H
 bool createRoot(Directory dir);        //创建目录，传入目录类
-bool createArticle(string iRoot,Article art);     //创建文章，传入目录索引和文章类
+bool changeRoot(string iRoot,Directory dir);        //目录重命名，传入目录类
+bool createArticle(string iRoot,Article art);     //创建文章，传入目录索引和文章类，
+bool changeArticle(string iRoot,string iArticle,Article art);//修改文章
 bool deleteRoot(string index);         //删除目录，传入目录索引
 bool deleteArticle(string iRoot,string iArticle); //删除文章，传入目录和文章索引
 vector<Directory> searchAllRoot();//查找所有目录，返回目录列表
 vector<Article> searchRootArticle(string iRoot);  //查找某个目录下所有文章，传入目录索引，返回文章列表
 vector<Article> searchAllArticle();  //返回所有文章
-Article searchArticle(string iRoot,string iArticle);  //查找某个目录下特定文章，传入目录和文章索引，返回文章类
+Article searchArticle(string iRoot,string iArticle);  //查找某个目录下特定文章，传入目录和文章索引，返回文章类,若找不到返回一个空对象。
 Json::Value returnRoot();  //返回json整个文件，若文件不存在，返回null
 bool writeInJson(Json::Value root);         //写入json文件
 #endif
