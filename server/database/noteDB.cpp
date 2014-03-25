f670a1cbecf555ac0a757b523a114a339d1eb389
slixurd <fatearcadia@gmail.com>
2014-03-25 18:33:03 +0800

    数据库部分基本完成

---
+++ server/database/noteDB.cpp
@@ -0,0 +1,204 @@
+/* 
+ * Author: slixurd(xiexiaopeng) 
+ */
+
+#include "../noteDB.h"
+
+NotesDB::NotesDB(){
+    mysql_init(&database);
+    //端口设置为0表示采用mysql的默认3306端口
+    port = 0;
+    this->host = CONFIG_HOST.c_str();
+    this->user = CONFIG_USER.c_str();
+    this->pass = CONFIG_PASS.c_str();
+    this->currentDB = CONFIG_DB.c_str();
+    if(!mysql_real_connect(&database,
+                    host.c_str(),
+                    user.c_str(),
+                    pass.c_str(),
+                    currentDB.c_str(),
+                    port,NULL,0
+                    )){
+        cerr<<"MYSQL 初始化失败."<<endl;
+        mysql_error(&database);
+    
+    }
+
+}
+NotesDB::NotesDB(string host,string user,string pass,string db){
+    mysql_init(&database);
+    //端口设置为0表示采用mysql的默认3306端口
+    port = 0;
+    this->host = host;
+    this->user = user;
+    this->pass = pass;
+    this->currentDB = db;
+
+    if(!mysql_real_connect(&database,
+                    host.c_str(),
+                    user.c_str(),
+                    pass.c_str(),
+                    db.c_str(),
+                    port,NULL,0
+                    )){
+        cerr<<"MYSQL 初始化失败."<<endl;
+        mysql_error(&database);
+    }
+
+}
+
+NotesDB::~NotesDB(){
+    mysql_close(&database);
+}
+
+/*
+ * 查询是否已经存在该用户名
+ * false表示该用户名不存在,可以注册
+ * true表示已有此名.
+ * 通过select的行数来判断
+ */
+
+bool NotesDB::check_user_exist(const string name){
+    
+    unsigned int len;
+    char query_sql[MAX_LEN];
+    char escape_sql[MAX_LEN];
+    mysql_real_escape_string(&database,escape_sql,name.c_str(),name.length()); 
+    string _name = escape_sql;
+    len = snprintf(query_sql,MAX_LEN,
+                "SELECT username FROM user WHERE username = '%s'",
+                _name.c_str());
+    int err = mysql_real_query(&database,query_sql,len);
+    if(err != 0)
+        cerr<<"database error";
+    else{
+    
+        MYSQL_RES* res = mysql_store_result(&database);
+        int num = mysql_num_rows(res); 
+        //cout<<"col: "<<mysql_field_count(&database)<<endl; 
+        //cout<<"row: "<<num<<endl;
+        mysql_free_result(res);
+        if(num == 0)
+            return false;
+        else 
+            return true;
+    }
+    return false;
+    
+}
+
+
+
+/*
+ * 添加用户,传入参数:用户名,密码.均不需要处理
+ * 如果数据库错误或者用户名已被使用无法注册,均返回false
+ */
+bool NotesDB::add_user(string name,string pass){
+    unsigned int len;
+    char query_sql[MAX_LEN];
+
+    bool exist = this->check_user_exist(name);
+    if(exist == true){
+        return false;
+    } 
+    char escape_sql[MAX_LEN];
+    mysql_real_escape_string(&database,escape_sql,name.c_str(),name.length()); 
+    string _name = escape_sql;
+    //生成随机盐
+    string salt = generate_salt();
+    //拼接密码并做hash
+    string _pass = sha1(pass + salt).substr(0,PASS_LEN);
+    len = snprintf(query_sql,MAX_LEN,
+                "INSERT INTO user(username,pass,salt) VALUE('%s','%s','%s')",
+                _name.c_str(),_pass.c_str(),salt.c_str());
+    cout<<query_sql<<endl;
+    int err = mysql_real_query(&database,query_sql,len);
+    if(err)
+       return false;
+    return true;
+    
+
+}
+/**
+ * 生成随机盐.通过时间和随机数混合以后再hash,取前15位做随机盐
+ */
+string NotesDB::generate_salt(){
+    
+    const time_t t = time(NULL);
+    srand(time(NULL));
+    string t_str;
+    stringstream ss;
+    ss<<t+(rand()%65535);
+    ss>>t_str;
+    string _salt = this->sha1(t_str).substr(0,15);
+    return _salt;
+
+}
+/***
+ * 获取随机盐前需要先查找用户是否存在
+ * 否则返回null
+ * 用于登陆时得到随机盐和密码hash
+ */
+string NotesDB::get_salt(const string name){
+    
+    unsigned int len;
+    char query_sql[MAX_LEN];
+    char escape_sql[MAX_LEN];
+    mysql_real_escape_string(&database,escape_sql,name.c_str(),name.length()); 
+    string _name = escape_sql;
+    len = snprintf(query_sql,MAX_LEN,
+                "SELECT salt FROM user WHERE username = '%s'",
+                _name.c_str());
+    int err = mysql_real_query(&database,query_sql,len);
+    if(err != 0)
+        cerr<<"database error";
+    MYSQL_RES* res = mysql_store_result(&database);
+    if(mysql_num_rows(res) == 0)
+        return NULL;
+    MYSQL_ROW row = mysql_fetch_row(res);
+    string _salt = row[0];
+    //cout<<"col: "<<mysql_field_count(&database)<<endl; 
+    //cout<<"row: "<<num<<endl;
+    mysql_free_result(res);
+    return _salt; 
+}
+
+/***
+ * 登陆,判断密码是否匹配
+ * 如果不存在用户名也返回false
+ * !!!时间允许还是应该在此处生成session
+ */
+bool NotesDB::login(string name,string pass){
+
+    unsigned int len;
+    char query_sql[MAX_LEN];
+    //如果用户名不存在,直接返回false
+    bool exist = this->check_user_exist(name);
+    if(exist == false){
+        return false;
+    } 
+    char escape_sql[MAX_LEN];
+    mysql_real_escape_string(&database,escape_sql,name.c_str(),name.length()); 
+    string _name = escape_sql;
+    //获取随机盐
+    string salt = this->get_salt(name);
+    string _pass = sha1(pass + salt).substr(0,PASS_LEN);
+    len = snprintf(query_sql,MAX_LEN,
+                "SELECT pass FROM user WHERE username = '%s'",
+                _name.c_str());
+    int err = mysql_real_query(&database,query_sql,len);
+    
+    if(err != 0)
+        cerr<<"database error";
+    MYSQL_RES* res = mysql_store_result(&database);
+    MYSQL_ROW row = mysql_fetch_row(res);
+    string correct_pass= row[0];
+    mysql_free_result(res);
+    cout<<"pass"<<row[0]<<endl; 
+    if(correct_pass == _pass)
+        return true;
+    else
+        return false;
+
+}
+
--- server/noteDB.h
+++ server/noteDB.h
@@ -3,11 +3,11 @@
  */
 #ifndef NOTE_DB
 #define NOTE_DB
+#include "config.h"
 
 #include <iostream>
 #include <map>
 #include <string>
-
 #include <cstdlib>
 #include <cstdio>
 #include <mysql/mysql.h>
@@ -18,27 +18,35 @@ using namespace std;
 
 class NotesDB{
 public:
+    //构造函数和析构函数
     NotesDB();
     NotesDB(string,string,string,string);
     ~NotesDB();
 
 
-    //增加用户
-    bool add_user(string,string);
-    bool login(string,string);
-    void init_all_data();
-    void pull_note();
-    void backup_note();
-    long create_note(string,string,long);
-    void save_note();
-    long create_dir(string,string);
-    void remove_dir(long);
-    bool remove_dir(string,long);
-    void sync_dir();
-    string sha1(const string&);
-    bool check_user_exist(const string);
-    bool check_node_exist(const long );
-    string get_salt(const string);
+    //用户注册和登陆
+    bool add_user(string, string);
+    bool login(string, string);
+    
+    
+    //笔记相关
+    long create_note(string, string, long);
+    int  update_note(string, string, long, long);
+    int  update_note(string, string,long);
+    int  update_note(long, long);
+    int  remove_note(long);
+    
+    int  get_note(long, ARTICLE_INFO*&);
+    int  get_notes_time(long,ARTICLE_SYNC*&);
+    int  get_brief(long, ARTICLE_INFO*&);
+    
+    //目录相关
+    long create_dir(string, string);
+    int  update_dir(string,long);
+    int  remove_dir(long);
+    int  remove_dir(string, long);
+    int  get_all_dir(string,DIR_INFO*&);
+
 private:
     MYSQL database;
     string host;
@@ -46,9 +54,16 @@ private:
     string pass;
     string currentDB;
     int port;
-
+    
+    //不开放的接口
+    string sha1(const string&);
+    string get_salt(const string);
     string generate_salt();
 
+    bool check_user_exist(const string);
+    bool check_node_exist(const long );    
+    int  change_dir(long, long);
+    int  remove_all_notes_from_dir(long);
 };
 
 
