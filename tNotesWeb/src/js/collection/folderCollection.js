013070c67403b9b3ce4bf214edcac00517a2d368
zhihuahuang <zhihuahuang.email@gmail.com>
2014-03-28 12:08:23 +0800

    增加 写入Folder 同步队列
    修改了一下代码，并增加了对Folder队列的操作。

--- tNotesWeb/src/js/collection/folderCollection.js
+++ tNotesWeb/src/js/collection/folderCollection.js
@@ -8,7 +8,7 @@ var FolderCollection = Backbone.Collection.extend({
     database: database,
     storeName: database.storeName,
 
-    currentID: 0,
+    selectedID: 0,
 	
 	initialize: function(){
 	},
@@ -19,27 +19,31 @@ var FolderCollection = Backbone.Collection.extend({
 
     /* 添加文章 */
     addNote: function(id){
-        if(this.currentID != 0)
-    	   this.get(this.currentID).addNote(id);
+        if(this.selectedID != 0)
+    	   this.get(this.selectedID).addNote(id);
     },
 
     /* 删除文章 */
     removeNote: function(id){
-        if(this.currentID != 0)
-           this.get(this.currentID).removeNote(id);
+        if(this.selectedID != 0)
+           this.get(this.selectedID).removeNote(id);
     },
 
      /* 清空文章 */
     clearNote: function(){
-        if(this.currentID != 0)
-           this.get(this.currentID).clearNote();
+        if(this.selectedID != 0)
+           this.get(this.selectedID).clearNote();
     },
 
     /* 更新修改时间 */
     updateModifiedTime: function(time){
-        if(this.currentID != 0)
-           this.get(this.currentID).updateModifiedTime(time);
-    }
+        if(this.selectedID != 0)
+           this.get(this.selectedID).updateModifiedTime(time);
+    },
+
+    getSelectedID: function(){
+        return this.selectedID;
+    },
 });
 
 // 创建folderCollection实例并获取数据
--- tNotesWeb/src/js/view/folderView.js
+++ tNotesWeb/src/js/view/folderView.js
@@ -1,11 +1,15 @@
 // 文章列表显示视图
 
-define(['folderCollection'], function (folderCollection) {
+define(['folderCollection', 'noteCollection', 'setting'], function (folderCollection, noteCollection, setting) {
 
 var FolderView = Backbone.View.extend({
 	//引用#folder,相当于View的关联对象
     el: $('#folder'),
     collection: folderCollection,
+    notes: noteCollection,
+
+    setting: setting,
+
     template: _.template($('#folder-template').html()),
 
     /*事件*/
@@ -15,14 +19,19 @@ var FolderView = Backbone.View.extend({
         'click #delete-folder-confirm-btn': 'deleteFolder', //删除文件夹事件
         'click #rename-folder-confirm-btn': 'renameFolder',  //重命名文件夹事件
         'click .folder': 'selectFolder',  //选中文件夹事件
-
     },
 
     initialize: function () {
     	_.bindAll(this, 'render');
+
         this.collection.bind('add', this.render);
         this.collection.bind('change', this.render);
         this.collection.bind('remove', this.render);
+
+        this.notes.bind('add', this.render);
+        this.notes.bind('change', this.render);
+        this.notes.bind('remove', this.render);
+
     },
 
 	/* 
@@ -32,7 +41,7 @@ var FolderView = Backbone.View.extend({
 	*/
     render: function(){
     	$('#folder-list').html(this.template({folders: this.collection.toJSON()}));
-    	$('#folder-list').find('[data-id="' + this.collection.currentID + '"]').addClass('active');
+    	$('#folder-list').find('[data-id="' + this.collection.selectedID + '"]').addClass('active');
     	return this;
     },
 
@@ -40,7 +49,13 @@ var FolderView = Backbone.View.extend({
     newFolder: function(){
     	var newFolderInput = $('#new-folder-name');
 
-    	this.collection.create({name: newFolderInput.val()}); //将new-floder-name获取到的名字插入collection
+    	var folder = this.collection.create({name: newFolderInput.val()}); //将new-floder-name获取到的名字插入collection
+
+        //将新建文章添加到新建列表
+        var addList = this.setting.get('folderAddedID');
+        addList.push(folder.get('id'));
+        this.setting.set({folderAddedID: addList});
+
     	newFolderInput.val("");
     },
 
@@ -48,15 +63,52 @@ var FolderView = Backbone.View.extend({
     renameFolder: function(){
     	var renameFolderInput = $('#rename-folder-name');
 
-    	this.collection.get(this.collection.currentID).set({name:(renameFolderInput.val()=="")?"重命名文件夹":renameFolderInput.val()});
+        var selectedID = this.collection.getSelectedID();
+
+    	this.collection.get(selectedID).set({name:(renameFolderInput.val()=="")?"重命名文件夹":renameFolderInput.val()});
         this.collection.updateModifiedTime();
+
+        
+        if(selectedID > 0){
+            var updateList = this.setting.get('folderUpdatedID');
+            if(updateList.indexOf(selectedID) < 0){
+                updateList.push(selectedID); //如果是服务器的目录才需要添加到 更改目录列表
+                this.setting.set({folderUpdatedID: updateList});
+            }
+        }
+
         renameFolderInput.val("");
+
     },
 
     /* 删除文件夹 */
     deleteFolder: function(){
-    	this.collection.get(this.collection.currentID).destroy();
-    	this.collection.currentID = 0;
+        var selectedID = this.collection.getSelectedID();
+
+    	this.collection.get(selectedID).destroy();
+
+        //将删除文章添加到列表
+        if(selectedID>0){
+            //如果ID>0，则为服务器文章，将其添加到删除列表
+            var updateList = this.setting.get('folderUpdatedID');
+            var index = updateList.indexOf(selectedID)
+            if(index > 0){
+                updateList.splice(index, 1);//如果修改列表中有该文章，也要删除
+                this.setting.set({folderUpdatedID: updateList});
+            }
+
+            var delelteList = this.setting.get('folderDeletedID');
+            delelteList.push(floder.get('id'));
+            this.setting.set({folderDeletedID: delelteList});
+        }
+        else if(this.collection.getCurrentFloderID()<0){
+            //如果ID<0，则为本地文章，将其从新建列表中删除
+            var addList = this.setting.get('folderAddedID');
+            addList.splice(addList.indexOf(floder.get('id')), 1);
+            this.setting.set({folderAddedID: addList});
+        }
+
+        this.collection.selectedID = 0;
     	
     	// 删除对话框隐藏
     	$('#deleteFolderModal').modal('hide');
@@ -68,13 +120,13 @@ var FolderView = Backbone.View.extend({
 
 	/* 选中文件夹 */
     selectFolder: function (event) {
-        if(this.collection.currentID == 0){
+        if(this.collection.selectedID == 0){
 	        // 恢复部分不可用按钮
 	        $('#rename-folder-button').removeClass('disabled').attr('data-toggle', 'modal');
 	        $('#delete-folder-btn').removeClass('disabled').attr('data-toggle', 'modal');
         }
 
-        this.collection.currentID = $(event.target).attr('data-id');
+        this.collection.selectedID = $(event.target).attr('data-id');
 
         this.render();
     },
