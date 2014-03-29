8a4db28be818b79c6f5faf3046e2dfdc4441365c
slixurd <fatearcadia@gmail.com>
2014-03-29 10:50:27 +0800

    sql

--- tnotes.sql
+++ tnotes.sql
@@ -1,41 +1,119 @@
-CREATE DATABASE tnotes;
-USE tnotes;
-CREATE TABLE user(
-    username varchar(80) NOT NULL,
-    pass varchar(80) NOT NULL,
-    salt varchar(80) NOT NULL,
-    session varchar(30),
-    PRIMARY KEY(username)
-)CHARSET=utf8;
+-- MySQL dump 10.13  Distrib 5.5.35, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: tnotes
+-- ------------------------------------------------------
+-- Server version	5.5.35-0ubuntu0.12.04.2
 
-CREATE TABLE node(
-    nodeID int(11) NOT NULL AUTO_INCREMENT,
-    parentID int(11) NOT NULL DEFAULT 0,
-    username varchar(80) NOT NULL,
-    name varchar(80) NOT NULL,
-    createTime timestamp DEFAULT CURRENT_TIMESTAMP,
-    modifiedTime timestamp ,
-    PRIMARY KEY(nodeID)
-)AUTO_INCREMENT = 10000;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `article`
+--
+
+DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `article` (
+  `articleID` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) NOT NULL,
+  `content` longtext,
+  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modifiedTime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`articleID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 
-CREATE TABLE article(
-    articleID int(11) NOT NULL AUTO_INCREMENT,
-    name varchar(80) NOT NULL,
-    context LONGTEXT,
-    createTime timestamp DEFAULT CURRENT_TIMESTAMP,
-    modifiedTime timestamp ,
-    PRIMARY KEY(articleID)
-);
+--
+-- Table structure for table `articleLocation`
+--
 
-CREATE TABLE articleLocation(
-    articleID int(11) NOT NULL,
-    nodeID int (11) NOT NULL
-);
+DROP TABLE IF EXISTS `articleLocation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `articleLocation` (
+  `articleID` int(11) NOT NULL,
+  `nodeID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE tmpArticle(
-    articleID int(11) NOT NULL,
-    name varchar(80) NOT NULL,
-    context LONGTEXT,
-    PRIMARY KEY(articleID)
-);
+
+--
+-- Table structure for table `node`
+--
+
+DROP TABLE IF EXISTS `node`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `node` (
+  `nodeID` int(11) NOT NULL AUTO_INCREMENT,
+  `parentID` int(11) NOT NULL DEFAULT '0',
+  `username` varchar(80) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modifiedTime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`nodeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+DROP TABLE IF EXISTS `tmpArticle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tmpArticle` (
+  `articleID` int(11) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `context` longtext,
+  PRIMARY KEY (`articleID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tmpArticle`
+--
+
+LOCK TABLES `tmpArticle` WRITE;
+/*!40000 ALTER TABLE `tmpArticle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tmpArticle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `username` varchar(80) NOT NULL,
+  `pass` varchar(80) NOT NULL,
+  `salt` varchar(30) NOT NULL,
+  `session` varchar(30) DEFAULT NULL,
+  `uncatagorized` int(11) DEFAULT '0',
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-03-29 10:47:33
